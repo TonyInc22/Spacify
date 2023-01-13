1651660f9a21923d40f91f4dbcaefbf3d8efc3c4
Anthony Incorvati <ari5086@psu.edu>
2023-01-13 01:31:00 -0500

    Sliders in position

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,7 +15,7 @@ SpacifyAudioProcessorEditor::SpacifyAudioProcessorEditor (SpacifyAudioProcessor&
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (600, 400);
+    setSize (700, 300);
 
     farOutMix.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
     farOutMix.setRange(0, 100, 1);
@@ -23,8 +23,23 @@ SpacifyAudioProcessorEditor::SpacifyAudioProcessorEditor (SpacifyAudioProcessor&
     farOutMix.setColour(juce::Slider::textBoxTextColourId, juce::Colours::black);
     farOutMix.setTextValueSuffix("% Mix");
     farOutMix.setValue(100);
-
     addAndMakeVisible(&farOutMix);
+
+    liftOffMix.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
+    liftOffMix.setRange(0, 100, 1);
+    liftOffMix.setTextBoxStyle(juce::Slider::TextBoxAbove, true, 75, 25);
+    liftOffMix.setColour(juce::Slider::textBoxTextColourId, juce::Colours::black);
+    liftOffMix.setTextValueSuffix("% Mix");
+    liftOffMix.setValue(100);
+    addAndMakeVisible(&liftOffMix);
+
+    otherWorldlyMix.setSliderStyle(juce::Slider::SliderStyle::LinearHorizontal);
+    otherWorldlyMix.setRange(0, 100, 1);
+    otherWorldlyMix.setTextBoxStyle(juce::Slider::TextBoxAbove, true, 75, 25);
+    otherWorldlyMix.setColour(juce::Slider::textBoxTextColourId, juce::Colours::black);
+    otherWorldlyMix.setTextValueSuffix("% Mix");
+    otherWorldlyMix.setValue(100);
+    addAndMakeVisible(&otherWorldlyMix);
 }
 
 SpacifyAudioProcessorEditor::~SpacifyAudioProcessorEditor()
@@ -46,5 +61,8 @@ void SpacifyAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
-    farOutMix.setBounds(getWidth() / 2 - 200, getHeight() / 2 - 100, 400, 200);
+
+    farOutMix.setBounds(25, 205, 200, 75);
+    liftOffMix.setBounds(250, 205, 200, 75);
+    otherWorldlyMix.setBounds(475, 205, 200, 75);
 }
